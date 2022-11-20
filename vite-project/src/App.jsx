d24d23effe5bbc2c7949c3a941136c2ddc71f636
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-11-19 21:49:36 -0500

    Add card dynamically

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css';
 import Navbar from './Navbar';
 import MainContent from './MainContent';
+import Cards from './Cards';
+
+import headshot from './assets/katie-zaferes.png'
+import star from './assets/star.png'
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
 			<MainContent />
+			<Cards
+				img={headshot}
+				sell="Sold out"
+				star={star}
+				rating="5.0"
+				span="(6) - USA"
+				desc="Life lessons with Katie Zaferes"
+				price="From $136"
+        per="/ person"
+			/>
 		</div>
 	);
 }
---
+++ vite-project/src/Cards.jsx
@@ -0,0 +1,23 @@
+export default function Cards(props) {
+	return (
+		<div className="card--one">
+			<div>
+				<img className="card-img-one" src={props.img} alt="" />
+				<h4 className="soldout">{props.sell}</h4>
+			</div>
+			<div className="stars">
+				<img className="star-img" src={props.star} alt="" />
+				<h4 className="stars-title">
+					{props.rating} <span className="grey">{props.span}</span>
+				</h4>
+			</div>
+
+			<div className="card-description">
+				<h3 className="title-offer one">{props.desc}</h3>
+				<h3 className="title-offer">
+					<span className="two">{props.price}</span> {props.per}
+				</h3>
+			</div>
+		</div>
+	);
+}
